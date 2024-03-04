a7636eb2059dde5c35340fb85b2061684c4e7ddf
avarboy <shamil.mutukhov@gmail.com>
2024-03-04 21:52:25 +0400

    send file

---
+++ age.js
@@ -0,0 +1,39 @@
+
+// task1
+
+// var age = prompt ("Enter your age")
+// var age2 =parseInt (age)
+
+// if (age2 > 18)
+
+// alert("You are old enough to drive a car");
+// else if ( age2 > 15  && age2 < 18) 
+// alert (" You are not old enough to drive a car")
+// else if (age2 > 10  && age2 < 15) 
+// alert ("You are still a child")
+
+
+// task2
+
+// var age = prompt ("Enter your age")
+// var age2 =parseInt (age)
+
+// if (age2 > 38)
+
+// console.log("Your are older than mine");
+// else if ( age2 > 30  && age2 < 38) 
+// console.log (" Your are younger than me ")
+
+
+
+
+//  task3
+
+//  var number = prompt('any number')
+//  var number2 =parseInt (number)
+
+
+//  if (number2 % 2 === 0) {
+//     alert('is an even number')
+// }else {  alert ('is an odd number')
+//  }
